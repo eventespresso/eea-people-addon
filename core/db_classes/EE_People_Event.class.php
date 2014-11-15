98735295871a99330b28cffaa12b39b56a41dce3
Darren Ethier <darren@roughsmootheng.in>
2014-11-14 22:05:13 -0500

    add EE_People_Event model object [touch: 3506]

---
+++ core/db_classes/EE_People_Event.class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file contains the class for the People Event Model object
+ *
+ * @since 1.0.0
+ * @package  EE People Addon
+ * @subpackage models
+ */
+if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+/**
+ *
+ * People Event model object class
+ *
+ * @since 1.0.0
+ *
+ * @package		EE People Addon
+ * @subpackage	models
+ * @author 		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class EE_People_Event extends EE_Base_Class {
+	/**
+	 *
+	 * @param type $props_n_values
+	 * @return EE_People_Event
+	 */
+	public static function new_instance( $props_n_values = array() ) {
+		$classname = __CLASS__;
+		$has_object = parent::_check_for_object( $props_n_values, $classname );
+		 return $has_object ? $has_object : new self( $props_n_values );
+	}
+
+
+	public static function new_instance_from_db ( $props_n_values = array() ) {
+		return  new self( $props_n_values, TRUE );
+	}
+}
+
+// End of file EE_People_Event.class.php

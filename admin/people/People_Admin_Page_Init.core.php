43bcf27c8cac94ae45bdde2437816623173a1873
Darren Ethier <darren@roughsmootheng.in>
2014-11-15 07:28:44 -0500

    finish people admin init [touch: 3507]

--- admin/people/People_Admin_Page_Init.core.php
+++ admin/people/People_Admin_Page_Init.core.php
@@ -1,51 +1,40 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+<?php
 /**
-* Event Espresso
-*
-* Event Registration and Management Plugin for WordPress
-*
-* @ package 		Event Espresso
-* @ author			Seth Shoultes
-* @ copyright 	(c) 2008-2011 Event Espresso  All Rights Reserved.
-* @ license 		{@link http://eventespresso.com/support/terms-conditions/}   * see Plugin Licensing *
-* @ link 				{@link http://www.eventespresso.com}
-* @ since		 	$VID:$
-*
-* ------------------------------------------------------------------------
-*
-* People_Admin_Page_Init class
-*
-* This is the init for the eea-people-addon Addon Admin Pages.  See EE_Admin_Page_Init for method inline docs.
-*
-* @package			Event Espresso (eea-people-addon addon)
-* @subpackage		admin/People_Admin_Page_Init.core.php
-* @author				Darren Ethier
-*
-* ------------------------------------------------------------------------
-*/
-class People_Admin_Page_Init extends EE_Admin_Page_Init  {
+ * This file contains the Admin Page Init for People
+ *
+ * @since 1.0.0
+ * @package  EE People Addon
+ * @subpackage admin
+ */
+if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
+/**
+ *
+ * People Admin Page init class
+ *
+ * @since 1.0.0
+ *
+ * @package		EE People Addon
+ * @subpackage	admin
+ * @author 		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
+ */
+class People_Admin_Page_Init extends EE_Admin_Page_CPT_Init  {
 
 	/**
-	 * 	constructor
-	 *
-	 * @access public
-	 * @return \People_Admin_Page_Init
+	 *  constructor
+	 * @since 1.0.0
 	 */
 	public function __construct() {
-
-		do_action( 'AHEE_log', __FILE__, __FUNCTION__, '' );
-
-		define( 'EEA-PEOPLE-ADDON_PG_SLUG', 'eea-people-addon' );
-		define( 'EEA-PEOPLE-ADDON_LABEL', __( 'eea-people-addon', 'event_espresso' ));
-		define( 'EEA_PEOPLE_ADDON_ADMIN_URL', admin_url( 'admin.php?page=' . EEA-PEOPLE-ADDON_PG_SLUG ));
+		define( 'EEA_PEOPLE_PG_SLUG', 'eea-people-addon' );
+		define( 'EEA_PEOPLE_LABEL', __( 'People Administration', 'event_espresso' ));
+		define( 'EEA_PEOPLE_ADDON_ADMIN_URL', admin_url( 'admin.php?page=' . EEA_PEOPLE_PG_SLUG ));
 		define( 'EEA_PEOPLE_ADDON_ADMIN_ASSETS_PATH', EEA_PEOPLE_ADDON_ADMIN . 'assets' . DS );
-		define( 'EEA_PEOPLE_ADDON_ADMIN_ASSETS_URL', EEA_PEOPLE_ADDON_URL . 'admin' . DS . 'eea-people-addon' . DS . 'assets' . DS );
+		define( 'EEA_PEOPLE_ADDON_ADMIN_ASSETS_URL', EEA_PEOPLE_ADDON_URL . 'admin' . DS . 'people' . DS . 'assets' . DS );
 		define( 'EEA_PEOPLE_ADDON_ADMIN_TEMPLATE_PATH', EEA_PEOPLE_ADDON_ADMIN . 'templates' . DS );
-		define( 'EEA_PEOPLE_ADDON_ADMIN_TEMPLATE_URL', EEA_PEOPLE_ADDON_URL . 'admin' . DS . 'eea-people-addon' . DS . 'templates' . DS );
-
+		define( 'EEA_PEOPLE_ADDON_ADMIN_TEMPLATE_URL', EEA_PEOPLE_ADDON_URL . 'admin' . DS . 'people' . DS . 'templates' . DS );
 		parent::__construct();
 		$this->_folder_path = EEA_PEOPLE_ADDON_ADMIN;
-
 	}
 
 
@@ -53,7 +42,7 @@ class People_Admin_Page_Init extends EE_Admin_Page_Init  {
 
 
 	protected function _set_init_properties() {
-		$this->label = EEA-PEOPLE-ADDON_LABEL;
+		$this->label = EEA_PEOPLE_LABEL;
 	}
 
 
@@ -70,9 +59,9 @@ class People_Admin_Page_Init extends EE_Admin_Page_Init  {
 			'menu_order' => 25,
 			'show_on_menu' => EE_Admin_Page_Menu_Map::BLOG_ADMIN_ONLY,
 			'parent_slug' => 'espresso_events',
-			'menu_slug' => EEA-PEOPLE-ADDON_PG_SLUG,
-			'menu_label' => EEA-PEOPLE-ADDON_LABEL,
-			'capability' => 'administrator',
+			'menu_slug' => EEA_PEOPLE_PG_SLUG,
+			'menu_label' => EEA_PEOPLE_LABEL,
+			'capability' => 'ee_read_peoples',
 			'admin_init_page' => $this
 		));
 	}
@@ -81,4 +70,4 @@ class People_Admin_Page_Init extends EE_Admin_Page_Init  {
 
 }
 // End of file People_Admin_Page_Init.core.php
-// Location: /wp-content/plugins/eea-people-addon/admin/eea-people-addon/People_Admin_Page_Init.core.php
+// Location: /wp-content/plugins/eea-people-addon/admin/people/People_Admin_Page_Init.core.php

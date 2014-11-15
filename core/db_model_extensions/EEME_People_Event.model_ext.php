4da861898bc4871987c38f00573fb84ed71154c6
Darren Ethier <darren@roughsmootheng.in>
2014-11-14 21:44:57 -0500

    add the model extension for Event [touch: 3506]

--- core/db_model_extensions/EEME_People_Event.model_ext.php
+++ core/db_model_extensions/EEME_People_Event.model_ext.php
@@ -1,33 +1,29 @@
 <?php
-
-if (!defined('EVENT_ESPRESSO_VERSION')) {
-	exit('No direct script access allowed');
-}
-
+/**
+ * This file contains the class for extending EEM Event to add people relationships
+ *
+ * @since 1.0.0
+ * @package  EE People Addon
+ * @subpackage models
+ */
+if ( ! defined('EVENT_ESPRESSO_VERSION')) exit('No direct script access allowed');
 /**
  *
- * EEME_Mock_Attendee extends EEM_Attendee and adds a function named 'new_func' onto it
+ * EEME_People_Event class
  *
- * @package			Event Espresso
- * @subpackage
- * @author				Mike Nelson
+ * @since 1.0.0
  *
+ * @package		EE People Addon
+ * @subpackage	models
+ * @author 		Darren Ethier
+ *
+ * ------------------------------------------------------------------------
  */
-class EEME_eea-people-addon_Attendee extends EEME_Base{
+class EEME_People_Event extends EEME_Base{
 	function __construct() {
-		$this->_model_name_extended = 'Attendee';
-		$this->_extra_tables = array(
-			'eea-people-addon_Attendee_Meta' => new EE_Secondary_Table('esp_eea-people-addon_attendee_meta', 'NATT_ID', 'ATT_ID')
-		);
-		$this->_extra_fields = array('eea-people-addon_Attendee_Meta'=>array(
-			'NATT_ID'=> new EE_DB_Only_Int_Field('NATT_ID', __('eea-people-addon Attendee Meta Row ID','event_espresso'), false),
-			'NATT_ID_fk'=>new EE_DB_Only_Int_Field('ATT_ID', __("Foreign Key to Attendee in Post Table", "event_espresso"), false),
-			'ATT_foobar'=>new EE_Foreign_Key_Int_Field('ATT_foobar', __("Foobar", 'event_espresso'), true,0,'People_CPT')));
-		$this->_extra_relations = array('People_CPT'=>new EE_Belongs_To_Relation());
+		$this->_model_name_extended = 'Event';
+		$this->_extra_relations = array('People'=>new EE_HABTM_Relation('People_Event' ) );
 		parent::__construct();
-	}
-	function ext_get_all_new_things( $arg1 = FALSE ){
-		return $this->_->get_all(array(array('People_CPT.NEW_ID'=>$arg1)));
 	}
 }
 

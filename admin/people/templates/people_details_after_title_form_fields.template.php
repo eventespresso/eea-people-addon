6163b0c1bd48d07c1e346c9e3a005310a38bdced
Darren Ethier <darren@roughsmootheng.in>
2014-12-07 14:44:59 -0500

    fix non-unique id selectors [touch: 7144]

--- admin/people/templates/people_details_after_title_form_fields.template.php
+++ admin/people/templates/people_details_after_title_form_fields.template.php
@@ -4,12 +4,12 @@ EEH_Template_Validator::verify_instanceof( $people, '$people', 'EE_Person');
  * @var $attendee - instance of EE_Attendee
  */
 ?>
-<div id="titlediv">
-	<div id="titlewrap">
+<div id="titlediv-people-addon">
+	<div id="titlewrap-people-addon">
 		<label class="hidden" id="attendee-first-name-text" for="PER_fname"><?php _e('First Name:', 'event_espresso'); ?></label>
 		<input type="text" class="smaller-text-field" name="PER_fname" value="<?php echo $people->get('PER_fname'); ?>" id="PER_fname" placeholder="<?php _e('First Name', 'event_espresso'); ?>" required>
 		<label class="hidden" id="attendee-first-name-text" for="PER_lname"><?php _e('Last Name:', 'event_espresso'); ?></label>
-		<input type="text" class="smaller-text-field" name="PER_lname" value="<?php echo $people->get('PER_lname'); ?>" id="PER_lname" placeholder="<?php _e('Last Name', 'event_espresso'); ?>" required>
+		<input id="PER_lname" type="text" class="smaller-text-field" name="PER_lname" value="<?php echo $people->get('PER_lname'); ?>" id="PER_lname" placeholder="<?php _e('Last Name', 'event_espresso'); ?>" required>
 		<div style="clear:both"></div>
 	</div>
 </div>

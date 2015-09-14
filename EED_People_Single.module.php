2e0caf295096dcdefc9d2138042787bbeef559a9
Brent Christensen <brent@eventespresso.com>
2015-09-14 15:55:54 -0700

    add $templates_path property and set within set_hooks(); add new method add_event_single_filters() as callback for action 'AHEE__EED_Event_Single__use_filterable_display_order__after_add_filters'; add new method filter_event_single_template_parts() as callback for filter 'FHEE__EED_Event_Single__set_config__template_parts'; add new method event_single_order_array() as callback for filter 'FHEE__EED_Event_Single__template_settings_form__event_single_order_array'; add new method event_single_template_settings_form() as callback for filter 'FHEE__EED_Event_Single__template_settings_form__templates'; add new method update_event_single_display_order_people() as callback for filter 'AHEE__EED_Event_Single__update_event_single_order__display_order_people'; add set_config()  method; use EED_People_Single::$templates_path in calls to EEH_Template::locate_template() for people addon templates; move over all methods from /public/template_hooks.php; use EE_Template_Part_Manager::add_template_part() if sortable single event tempalte parts is being used; [touch:8831]

--- EED_People_Single.module.php
+++ EED_People_Single.module.php
@@ -24,6 +24,9 @@
  */
 class EED_People_Single extends EED_Module {
 
+	public static $templates_path;
+
+
 
 	/**
 	 * @return EED_People_Single
@@ -42,6 +45,9 @@ class EED_People_Single extends EED_Module {
 	  */
 	 public static function set_hooks() {
 		 EE_Config::register_route( 'person', 'People_Single', 'run' );
+		 EED_People_Single::$templates_path = EEA_PEOPLE_ADDON_PATH . 'public' . DS . 'templates' . DS;
+		 add_action( 'AHEE__EED_Event_Single__use_filterable_display_order__after_add_filters', array( 'EED_People_Single', 'add_event_single_filters' ), 10 );
+		 add_filter( 'FHEE__EED_Event_Single__set_config__template_parts', array( 'EED_People_Single', 'filter_event_single_template_parts' ), 10 );
 	 }
 
 	 /**
@@ -51,14 +57,70 @@ class EED_People_Single extends EED_Module {
 	  *  @return 	void
 	  */
 	 public static function set_hooks_admin() {
+		 // EED_Event_Single
+		 add_filter( 'FHEE__EED_Event_Single__template_settings_form__event_single_order_array', array( 'EED_People_Single', 'event_single_order_array' ), 10 );
+		 add_filter( 'FHEE__EED_Event_Single__template_settings_form__templates', array( 'EED_People_Single', 'event_single_template_settings_form' ), 10 );
+		 add_action( 'AHEE__EED_Event_Single__update_event_single_order__display_order_people', array( 'EED_People_Single', 'update_event_single_display_order_people' ), 10 );
 	 }
 
 
 
+	/**
+	 *    set_config
+	 *
+	 * @return \EE_People_Config
+	 */
+	public function set_config() {
+		$this->set_config_section( 'template_settings' );
+		$this->set_config_class( 'EE_People_Config' );
+		$this->set_config_name( 'EED_People_Single' );
+		return $this->config();
+	}
 
 
 
-	 /**
+	/**
+	 *    event_single_order_array
+	 *
+	 * @access    public
+	 * @param  array $event_single_order_array
+	 * @return    array
+	 */
+	public static function event_single_order_array( $event_single_order_array ) {
+		$event_single_order_array[ EED_People_Single::instance()->set_config()->event_single_display_order_people ] = 'people';
+		return $event_single_order_array;
+	}
+
+
+
+	/**
+	 *    event_single_template_settings_form
+	 *
+	 * @access    public
+	 * @param  array $template_settings_form
+	 * @return    array
+	 */
+	public static function event_single_template_settings_form( $template_settings_form ) {
+		$template_settings_form[ 'people' ] = __( "People", "event_espresso" );
+		return $template_settings_form;
+	}
+
+
+
+	/**
+	 *    update_event_single_template_settings_form
+	 *
+	 * @access 	public
+	 * @param 	int $priority
+	 * @return 	array
+	 */
+	public static function update_event_single_display_order_people( $priority ) {
+		EED_People_Single::instance()->set_config()->event_single_display_order_people = $priority;
+	}
+
+
+
+	/**
 	  *    run - initial module setup
 	  *
 	  * @access    public
@@ -66,7 +128,7 @@ class EED_People_Single extends EED_Module {
 	  * @return    void
 	  */
 	 public function run( $WP ) {
-	 	add_filter( 'template_include', array( $this, 'template_include' ), 999 );
+		 add_filter( 'template_include', array( $this, 'template_include' ), 999 );
 	 	EE_Registry::instance()->load_helper( 'People_View' );
 	 }
 
@@ -95,9 +157,8 @@ class EED_People_Single extends EED_Module {
 	 */
 	public function template_include( $template ) {
 		global $post;
-
 		// not a custom template?
-		if ( EE_Registry::instance()->load_core( 'Front_Controller', array(), false, true )->get_selected_template() != 'single-espresso_people.php' && ! post_password_required( $post ) ) {
+		if ( EE_Registry::instance()->load_core( 'Front_Controller', array(), false )->get_selected_template() != 'single-espresso_people.php' && ! post_password_required( $post ) ) {
 			EEH_Template::load_espresso_theme_functions();
 			//add extra people data
 			add_filter( 'the_content', array( 'EED_People_Single', 'person_details' ), 100 );
@@ -118,16 +179,87 @@ class EED_People_Single extends EED_Module {
 	public static function person_details( $content ) {
 		global $post;
 		if ( is_single() && $post->post_type == 'espresso_people' && ! post_password_required() ) {
-			$content .= EEH_Template::locate_template( 'content-espresso_people-details.php' );
+			$content .= EEH_Template::locate_template( EED_People_Single::$templates_path . 'content-espresso_people-details.php' );
 		}
 		return $content;
 	}
 
 
 
+	/*********************************** following moved over from /public/template_hooks.php ***********************************/
+
+
+
 	/**
-	 *		@ override magic methods
-	 *		@ return void
+	 * add_event_single_filters
+	 *
+	 * @return void
+	 */
+	public static function add_event_single_filters() {
+		//hook into event details right before event details content
+		add_filter( 'the_content', array( __CLASS__, 'people_event_details' ), 100 );
+	}
+
+
+
+	/**
+	 * Registers the folder for core people templates to be included with the template path locator.
+	 * Note: To customize, just copy the template from /public/templates/* and put in your theme folder.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $template_paths incoming paths
+	 * @return array
+	 */
+	public static function add_template_folder_to_paths( $template_paths ) {
+		$template_paths[] = EEA_PEOPLE_ADDON_PATH . 'public/templates/';
+		return $template_paths;
+	}
+
+
+
+	/**
+	 * filter_event_single_template_parts
+	 *
+	 * @param EE_Template_Part_Manager $template_parts
+	 * @return array
+	 */
+	public static function filter_event_single_template_parts( EE_Template_Part_Manager $template_parts ) {
+		EED_People_Single::instance()->set_config();
+		$config = EED_People_Single::instance()->config();
+		if ( $config instanceof EE_People_Config ) {
+			$config->event_single_display_order_people = isset( $config->event_single_display_order_people ) ? $config->event_single_display_order_people : 115;
+			$template_parts->add_template_part(
+				'people',
+				EED_People_Single::$templates_path . 'content-espresso_events-people.php',
+				$config->event_single_display_order_people
+			);
+		}
+		return $template_parts;
+	}
+
+
+
+	/**
+	 * This is added right before event content is displayed for an event
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $content (the content so far).
+	 *
+	 * @return string show people attached to an event.
+	 */
+	public static function people_event_details( $content ) {
+		return $content . EEH_Template::locate_template( EED_People_Single::$templates_path . 'content-espresso_events-people.php' );
+	}
+
+
+	/**
+	 *        @ override magic methods
+	 *        @ return void
+	 * @param $a
+	 * @param $b
+	 * @return bool
 	 */
 	public function __set($a,$b) { return FALSE; }
 	public function __get($a) { return FALSE; }

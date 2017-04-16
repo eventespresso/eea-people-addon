66a37813975426cd6d3bea5ed0e144727832a0de
Darren Ethier <darren@roughsmootheng.in>
2017-04-16 15:39:07 -0400

    conditionally set core branch being tested with on the presence of a `$BUILD_BRANCH` environment variable [touch: 10623
    - again testing with the people add-on first.

--- tests/bin/circle-build.sh
+++ tests/bin/circle-build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+## if there is a BUILD_BRANCH build environment variable then we use that for what branch of
+## ee core to checkout.  otherwise master.
+if [ -n "$BUILD_BRANCH" ]; then
+    core_branch=$BUILD_BRANCH
+else
+    core_branch="master"
+fi
+
+# commands taking care of WordPress setup
 function wpCoreSetup {
     git clone git://develop.git.wordpress.org/ $WP_CORE_DIR
     cd $WP_CORE_DIR
@@ -8,20 +18,25 @@ function wpCoreSetup {
     sed -i "s/yourpasswordhere//" wp-tests-config.php
 }
 
+# commands taking care of ee core setup
+# receives an argument indicating what branch to checkout.
 function eeCoreSetup {
     local BRANCH=$1
     git clone git@github.com:eventespresso/event-espresso-core.git $event_espresso_core_dir
     git checkout $BRANCH
 }
 
+# commands taking care of addon setup
 function addOnSetup {
     mv $plugin_loc $plugin_dir
 }
 
+# commands taking care of creating the WordPress test db.
 function createDB {
     mysql -e 'CREATE DATABASE wordpress_test;' -uroot;
 }
 
+# commands taking care of setting up phpunit
 function setupPhpUnit {
     wget --no-check-certificate https://phar.phpunit.de/phpunit-old.phar
     chmod +x phpunit-old.phar
@@ -29,7 +44,7 @@ function setupPhpUnit {
 }
 
 wpCoreSetup
-eeCoreSetup master
+eeCoreSetup $core_branch
 addOnSetup
 createDB
 setupPhpUnit

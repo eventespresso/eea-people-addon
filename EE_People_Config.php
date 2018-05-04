2974157d1cac63f5510ce2f82f31a6003da1988e
Darren Ethier <darren@roughsmootheng.in>
2018-05-04 11:43:58 -0400

    Add build tools configuration (#12)
    * Add build configuration files
    
    * fix phpcs violations

--- EE_People_Config.php
+++ EE_People_Config.php
@@ -1,50 +1,30 @@
-<?php if ( ! defined('EVENT_ESPRESSO_VERSION')) { exit('No direct script access allowed'); }
+<?php
 /**
- * Event Espresso
- *
- * Event Registration and Ticketing Management Plugin for WordPress
- *
- * @ package			Event Espresso
- * @ author			    Event Espresso
- * @ copyright		(c) 2008-2014 Event Espresso  All Rights Reserved.
- * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
- * @ link					http://www.eventespresso.com
- *
- *
- * ------------------------------------------------------------------------
- */
- /**
  *
  * Class EE_People_Config
  *
  * Description
  *
- * @package         Event Espresso
- * @subpackage    core
- * @author				Brent Christensen
+ * @package               Event Espresso
+ * @subpackage            core
+ * @author                Brent Christensen
  *
  *
  */
 
-class EE_People_Config extends EE_Config_Base {
+class EE_People_Config extends EE_Config_Base
+{
+
+    public $event_single_display_order_people;
+    public $event_archive_display_order_people;
 
 
-  public $event_single_display_order_people;
-  public $event_archive_display_order_people;
-
-
-
-  /**
-   *    class constructor
-   */
-  public function __construct() {
-   $this->event_single_display_order_people = 125;
-   $this->event_archive_display_order_people = 125;
-  }
-
+    /**
+     *    class constructor
+     */
+    public function __construct()
+    {
+        $this->event_single_display_order_people = 125;
+        $this->event_archive_display_order_people = 125;
+    }
 }
-
-
-
-// End of file EE_People_Config.php
-// Location: /wp-content/plugins/eea-people-addon/EE_People_Config.php
